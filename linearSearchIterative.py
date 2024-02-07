6a2df6a16aaae5efccb7bb5d8b9f0cf5b1af413c
alok <112388516+alokumarjaiswal@users.noreply.github.com>
2024-02-07 19:23:25 +0530

    hi

--- linearSearchIterative.py
+++ linearSearchIterative.py
@@ -1,12 +1,13 @@
-def linearSearch(array, key):
+def linearSearchIterative(array, key):
     for i in range(len(array)):
         if array[i] == key:
             return i
     return -1
 
+
 array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 key = 5
-result = linearSearch(array, key)
+result = linearSearchIterative(array, key)
 if result != -1:
     print("Element found at index", result)
 else:
